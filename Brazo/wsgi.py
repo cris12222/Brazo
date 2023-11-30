6cb86dcc543dc69457718359563cdcb997b79f9f
cris12222 <git@github.com:cris12222>
2023-11-30 10:11:09 -0700

    first commit

---
+++ Brazo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Brazo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Brazo.settings')
+
+application = get_wsgi_application()
